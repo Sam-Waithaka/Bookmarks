35c0dcf61231df755dba610635117ca1acd00dba
Sam <sw.waithakasamuel@gmail.com>
2024-10-22 10:54:57 +0300

    Add User Registration

--- config/account/views.py
+++ config/account/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
-# from .forms import LoginForm
+from .forms import UserRegistrationForm, LoginForm
 
 # Create your views here.
 
@@ -31,3 +31,18 @@ from django.shortcuts import render
 @login_required
 def dashboard(request):
     return render( request, 'account/dashboard.html', {'section':'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':  # Correct the case here
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    
+    return render(request, 'account/register.html', {'user_form': user_form})  # Ensure this runs in both GET and invalid POST cases
